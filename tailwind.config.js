09ce4ae0c11e34424526a710931d60dc0054549a
gonzalo <gonzalojpv@gmail.com>
2023-06-11 15:54:45 -0600

    GP, tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        green: "#42b883",
+        "gray-dark": "#0f172a",
+      },
+    },
+  },
+  plugins: [],
+};
+
