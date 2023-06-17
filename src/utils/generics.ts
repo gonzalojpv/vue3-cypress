a5befed3500d1fbc662567f41591e658a0a24bfc
gonzalo <gonzalojpv@gmail.com>
2023-06-17 00:47:06 -0600

    GP, generic class and generic interface

--- src/utils/generics.ts
+++ src/utils/generics.ts
@@ -3,3 +3,31 @@ export function log<T, V>(a: T, b: V): V {
 
   return b
 }
+
+export async function fetchData<T>(resource: string): Promise<T> {
+  const result = await window.fetch(`${resource}`)
+  return result.json()
+}
+
+export class Developer<T> {
+  computer: T
+
+  constructor(t: T) {
+    this.computer = t
+  }
+}
+// interfaces generic
+interface KeyValue<T, V> {
+  key: T
+  value: V
+}
+interface Product {
+  id: string
+}
+
+export function fetchProduct(): KeyValue<string, Product> {
+  return {
+    key: 'product id',
+    value: { id: 'product id' },
+  }
+}
--- src/views/pages/typescript/generics.vue
+++ src/views/pages/typescript/generics.vue
@@ -1,7 +1,36 @@
 <script setup lang="ts">
-import { log } from '@/utils/generics'
+import { onMounted } from 'vue'
+import { log, fetchData, Developer } from '@/utils/generics'
+
+type Joke = {
+  setup: string
+  delivery: string
+}
+
+type Computer = {
+  turnOn: () => void
+  turnOff: () => void
+}
 
 log<string, number>('data', 42)
 log<string, string>('data', 'happy day')
+log(1, 2)
+log('Regards', 'Hi world')
+// Generic class
+const developer = new Developer<Computer>({
+  turnOn: () => {
+    console.log('turnOn')
+  },
+  turnOff: () => {
+    console.log('turnOff')
+  },
+})
+
+console.log('Generic Class', developer.computer)
+onMounted(async () => {
+  const joke = await fetchData<Joke>('https://v2.jokeapi.dev/joke/christmas')
+
+  console.log('joke', joke.delivery)
+})
 </script>
 <template>TypeScript - Generic</template>
