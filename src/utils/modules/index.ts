f3ada16e0e70705f51dfb462d93bec17b92f4c5b
gonzalo <gonzalojpv@gmail.com>
2023-06-17 23:36:45 -0600

    GP, export and import

---
+++ src/utils/modules/index.ts
@@ -0,0 +1,8 @@
+import { Group } from './Group'
+import { Point } from './Point'
+
+const point = new Point(1, 2)
+console.log('Point', point)
+
+const group = new Group(23, 'text')
+console.log('Group', group)
