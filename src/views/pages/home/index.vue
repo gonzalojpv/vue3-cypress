677df3e04e073ada64f3e4acf65f6dfb17b6a0e3
gonzalo <gonzalojpv@gmail.com>
2023-06-19 14:56:58 -0600

    GP, decorators

--- src/views/pages/home/index.vue
+++ src/views/pages/home/index.vue
@@ -66,7 +66,7 @@
             <p
               class="mt-0.5 whitespace-nowrap rounded-md bg-gray-50 px-1.5 py-0.5 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10"
             >
-              In progress
+              Completed
             </p>
           </div>
           <div
@@ -81,6 +81,33 @@
             class="hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block"
           >
             View project<span class="sr-only">, TypeScript- Generic</span>
+          </router-link>
+        </div>
+      </li>
+      <li class="flex items-center justify-between gap-x-6 py-5">
+        <div class="min-w-0">
+          <div class="flex items-start gap-x-3">
+            <p class="text-sm font-semibold leading-6 text-gray-900">
+              TypeScript- Decorators
+            </p>
+            <p
+              class="mt-0.5 whitespace-nowrap rounded-md bg-gray-50 px-1.5 py-0.5 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10"
+            >
+              In progress
+            </p>
+          </div>
+          <div
+            class="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500"
+          >
+            <p class="truncate">Created by Gonzalo</p>
+          </div>
+        </div>
+        <div class="flex flex-none items-center gap-x-4">
+          <router-link
+            :to="{ name: 'typescript.decorator' }"
+            class="hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block"
+          >
+            View project<span class="sr-only">, TypeScript- Decorators</span>
           </router-link>
         </div>
       </li>
