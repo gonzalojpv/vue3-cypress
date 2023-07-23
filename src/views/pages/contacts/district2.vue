e3896d575bba85db0812a39bb66e8a4c8e5a174b
gonzalo <gonzalojpv@gmail.com>
2023-07-22 23:53:33 -0600

    GP, UI:form

--- src/views/pages/contacts/district2.vue
+++ src/views/pages/contacts/district2.vue
@@ -1,100 +1,127 @@
 <template>
   <div class="container mx-auto mt-28 flex justify-between">
     <div>
-      <h2 class="font-montserrat text-6xl">Let's collaborate</h2>
-      <small>link</small>
-      <div class="flex justify-between">
+      <h1 class="font-montserrat text-6xl text-dark-gray">Let's collaborate</h1>
+      <a
+        href="mailto:gonzalojpv@gmail.com"
+        class="color-gray block text-base hover:text-gray-dark"
+        >gonzalojpv@gmail.com</a
+      >
+      <div class="mt-10 flex justify-between">
         <div>
           <span class="mb-2 block">Find us</span>
           <ul class="grid grid-cols-4 gap-4">
             <li class="inline-block">
-              <a href="#" class="color-gray text-sm font-light">FB</a>
+              <a
+                href="#"
+                class="text-base font-light text-gray hover:text-gray-dark"
+                >FB</a
+              >
             </li>
             <li>
-              <a href="#" class="color-gray text-sm font-light">IG</a>
+              <a
+                href="#"
+                class="color-gray text-base font-light hover:text-gray-dark"
+                >IG</a
+              >
             </li>
             <li>
-              <a href="#" class="color-gray text-sm font-light">BE</a>
+              <a
+                href="#"
+                class="color-gray text-base font-light hover:text-gray-dark"
+                >BE</a
+              >
             </li>
             <li>
-              <a href="#" class="color-gray text-sm font-light">IN</a>
+              <a
+                href="#"
+                class="color-gray text-base font-light hover:text-gray-dark"
+                >IN</a
+              >
             </li>
           </ul>
         </div>
         <div>
           <span class="mb-2 block">Bahlum Code</span>
-          <small class="color-gray block font-light">+52 (0) 2836361354</small>
+          <small
+            class="block text-base font-light text-gray hover:text-gray-dark"
+            >+52 (0) 2836361354</small
+          >
         </div>
       </div>
     </div>
     <div>
-      <h3>Say hello</h3>
+      <h3 class="mb-6 text-3xl font-medium text-dark-gray">Say hello</h3>
       <form action="" class="font-montserrat">
         <div class="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2">
           <div>
             <label
               for=""
-              class="text-gray-900 block text-sm font-medium leading-6"
+              class="block text-sm font-medium uppercase leading-6 text-dark-gray"
               >Name</label
             >
             <input
               type="text"
               placeholder="Your name"
-              class="text-gray-900 ring-gray-300 placeholder:text-gray-400 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              class="hover:border-dak-gray focus:border-dak-gray hover:border-b-1 focus:border-b-1 border-b-1 block w-full border-x-0 border-t-0 border-white px-0 text-base font-light text-dark-gray shadow-sm ring-0 ring-inset ring-inherit placeholder:text-gray focus:ring-0 focus:ring-inset"
             />
           </div>
           <div>
             <label
               for=""
-              class="text-gray-900 block text-sm font-medium leading-6"
+              class="block text-sm font-medium uppercase leading-6 text-dark-gray"
               >Subject</label
             >
             <input
               type="text"
               placeholder="Choose subject"
-              class="text-gray-900 ring-gray-300 placeholder:text-gray-400 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              class="hover:border-dak-gray focus:border-dak-gray hover:border-b-1 focus:border-b-1 border-b-1 block w-full border-x-0 border-t-0 border-white px-0 text-base font-light text-dark-gray shadow-sm ring-0 ring-inset ring-inherit placeholder:text-gray focus:ring-0 focus:ring-inset"
             />
           </div>
           <div>
             <label
               for=""
-              class="text-gray-900 block text-sm font-medium leading-6"
+              class="block text-sm font-medium uppercase leading-6 text-dark-gray"
               >Company</label
             >
             <input
               type="text"
               placeholder="Choose subject"
-              class="text-gray-900 ring-gray-300 placeholder:text-gray-400 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              class="hover:border-dak-gray focus:border-dak-gray hover:border-b-1 focus:border-b-1 border-b-1 block w-full border-x-0 border-t-0 border-white px-0 text-base font-light text-dark-gray shadow-sm ring-0 ring-inset ring-inherit placeholder:text-gray focus:ring-0 focus:ring-inset"
             />
           </div>
           <div>
             <label
               for=""
-              class="text-gray-900 block text-sm font-medium leading-6"
+              class="block text-sm font-medium uppercase leading-6 text-dark-gray"
               >Email</label
             >
             <input
-              type="text"
+              type="email"
               placeholder="Choose subject"
-              class="text-gray-900 ring-gray-300 placeholder:text-gray-400 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              class="hover:border-dak-gray focus:border-dak-gray hover:border-b-1 focus:border-b-1 border-b-1 block w-full border-x-0 border-t-0 border-white px-0 text-base font-light text-dark-gray shadow-sm ring-0 ring-inset ring-inherit placeholder:text-gray focus:ring-0 focus:ring-inset"
             />
           </div>
           <div class="col-span-2">
             <label
               for=""
-              class="text-gray-900 block text-sm font-medium leading-6"
+              class="block text-sm font-medium uppercase leading-6 text-dark-gray"
               >Message</label
             >
             <textarea
               id=""
               name=""
-              cols="30"
-              rows="10"
-              class="text-gray-900 ring-gray-300 placeholder:text-gray-400 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Start typing here"
+              class="hover:border-dak-gray focus:border-dak-gray hover:border-b-1 focus:border-b-1 border-b-1 block w-full border-x-0 border-t-0 border-white px-0 text-base font-light text-dark-gray shadow-sm ring-0 ring-inset ring-inherit placeholder:text-gray focus:ring-0 focus:ring-inset sm:leading-6"
             ></textarea>
           </div>
           <div class="col-span-2">
-            <button type="submit">Submit</button>
+            <button
+              type="submit"
+              class="font-base font-light underline decoration-solid"
+            >
+              Submit
+            </button>
           </div>
         </div>
       </form>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,13 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,vue}'],
   theme: {
     extend: {
+      textColor: {
+        mostly: '#5d8aa8',
+      },
       colors: {
         green: '#42b883',
         'gray-dark': '#0f172a',
-        'dark-gary': '#212121',
+        'dark-gray': '#212121',
         gray: '#575757',
         'light-gray': '#f7f7f7',
         'pure-white': '#fff',
